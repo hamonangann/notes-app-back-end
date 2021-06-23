336efd3a5a9279bbcd7b35605f526e2ba45521f0
Bornyto Hamonangan <hamonangan.nito@gmail.com>
2021-06-23 21:56:39 +0700

    implementasi postgres

---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable camelcase */
+/* eslint-disable no-underscore-dangle */
+
+const mapDBToModel = ({
+  id,
+  title,
+  body,
+  tags,
+  created_at,
+  updated_at,
+}) => ({
+  id,
+  title,
+  body,
+  tags,
+  createdAt: created_at,
+  updatedAt: updated_at,
+});
+
+module.exports = { mapDBToModel };
